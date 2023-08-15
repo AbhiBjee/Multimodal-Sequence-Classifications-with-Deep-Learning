6ae8fde7954c11abdaed696d47ba9f50fd6922c3
Abhinaba Bhattacharjee <abhinaba.bjee@gmail.com>
2023-08-15 01:41:17 -0400

    Updated commentd on  intraClinicianDataOrgRNNTrain.m file

--- IntraClinicianDataOrgRNNTrain.m
+++ IntraClinicianDataOrgRNNTrain.m
@@ -1,7 +1,19 @@
+% Created by Abhinaba Bhattacharjee, Purdue University, 6/29/2023
+
+% This code is used to train and test force based hand manipulation motion sequences used in Manual Therapy as performed by an individual experienced clinicians. 
+% The Data acquired from the manipulating the left side of the body is selected for training while that of the right side is selected for testing. 
+% The Raw Data contains Triaxial Forces, IMU data (Accelerometer, Gyroscope and/or Magnetometer Data), which are segregated into Training Vector and Testing Vector 
+% for deep learning model (LSTM-RNN) training and multimodal sequence prediction.
+%
+%The Dataset is labelled with atleast 2 categories, which are either ("Strumming" and "Random N/A") to perform linear stroke based massage 
+% or ("JStroke" and "Random N/A") curved motion based massage on different anatomical regions of the body especially - Thoracolumbar, Upper Thigh and Calf regions
+
 %% workspace clear
 clc;
 clear all;
 %% load data files
+% Select either of C_11 or C_1 in the Foldername 'ClinicalData' in the currect working directory. 
+
 path = uigetdir('/Users/user/');
 files = dir(fullfile(path,'**','*.csv'));
 totalsize = length(files);
@@ -22,11 +34,11 @@ for i = 1:totalsize
 end
 
 %% Prepare training and testing data files
+% Data files sequence column indices:
+%Time, 2-FrcX, 3-FrcY, 4-FrcZ, 5-FrcRez, 6-Yaw, 7-Pitch, 8-Roll, 9-AccX, 
+% 10-AccY,11-AccZ,12-GyroRez
 
-%Time, 2-FrcX, 3-FrcY, 4-FrcZ, 5-FrcRez, 6-Yaw, 7-Pitch, 8-Roll, 9-AccX, 10-AccY,11-AccZ,
-%12-GyroRez
-
-Features = [2:4,9:11,12];
+Features = [2:4,9:11,12];% Define the 7 characterisitic features (Triaxial forces, Triaxial Acceleration and GyroRMS) 
 
 %%%%% training data files
 xTrain = cell(1,1);
@@ -73,12 +85,26 @@ options = trainingOptions('adam', ...
 %%
 net = trainNetwork(xTrain,yTrain,layers,options);
 
-%% Classify the test data
-% clear xTest, yTest;
-% xTest{1}(:,:) = [dataFile{4,2}(:,Features)].';
-% yTest{1}(:,:) = [dataLblFile{4,2}].';
+%% Classify the test data and view the sensitivity and ROC metrics
 
-YPred = classify(net,xTest{1});
+[YPred,scores] = classify(net,xTest{1});
 %%% Accuracy 
 acc = sum(YPred == yTest{1})./numel(yTest{1})
 plotconfusion(yTest{1},YPred)
+classes = categories(YPred);
+
+figure
+hold on
+%accuracy = mean(YPred == yTest{1});
+rocObj = rocmetrics(yTest{1},scores.',categorical(classes));
+[FPR1,TPR1,Thresholds1,AUC1] = average(rocObj,"macro");
+[FPR2,TPR2,Thresholds2,AUC2] = average(rocObj,"micro");
+
+subplot(1,2,1)
+plot(rocObj,AverageROCType="macro", ClassNames=[])
+subplot(1,2,2)
+plot(rocObj,AverageROCType="micro", ClassNames=[])
+hold off
+%legend(ClassNames,'Location','northwest')
+str = sprintf("Accuracy Metrics macro AUC Macro=%0.2f, AUC Micro = %0.2f, Mean True Positive Rate  = %0.4f ", AUC1, AUC2, mean(TPR2));
+disp(str);
